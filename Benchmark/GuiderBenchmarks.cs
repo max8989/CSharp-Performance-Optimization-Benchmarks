7dfecb6c73bd64dcb5d893a3090bacc52152bea6
Maxime Gagne <max.8989@hotmail.com>
2023-01-13 07:17:59 -0500

    Renaming

---
+++ Benchmark/GuiderBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SpanBenchmark
+{
+    [MemoryDiagnoser(false)]
+	public class GuiderBenchmarks
+	{
+        private static readonly Guid TestGuidId = Guid.Parse("25fa07b1-cebd-4c78-b23a-a17b3f4845ce");
+        private const string TestIdAsString = "sQf6Jb3OeEyyOqF7P0hFzg";
+
+        [Benchmark]
+        public Guid ToGuidFromString()
+        {
+            return Guider.ToGuidFromString(TestIdAsString);
+        }
+
+        [Benchmark]
+        public Guid ToGuidFromStringOptimized()
+        {
+            return Guider.ToGuidFromStringOp(TestIdAsString);
+        }
+
+        [Benchmark]
+        public string ToStingFromGuid()
+        {
+            return Guider.ToStringFromGuid(TestGuidId);
+        }
+    }
+}
+
---
+++ Benchmark/Program.cs
@@ -0,0 +1,5 @@
+﻿using BenchmarkDotNet.Running;
+using SpanBenchmark;
+
+BenchmarkRunner.Run<GuiderBenchmarks>();
+
